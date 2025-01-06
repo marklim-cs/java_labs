abb0e772f7a43927eb05fdbb65fc2ddb4e16fd57
Marina <climovicimarina@gmail.com>
2025-01-06 17:30:58 +0100

    screen output

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main (String[] args) {
+        String name = "John";
+        // println is commonly used
+        System.out.println("Hello, ", name);
+        System.out.print("I print ->");
+        System.out.println(" on the same line");
+        System.out.println(3+3);
+
+    }
+
+
+
+}
