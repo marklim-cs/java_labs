1c0d317f014821d40f48a564fb4914ed576d9aab
Marina <climovicimarina@gmail.com>
2025-01-19 18:31:03 +0100

    switch, break

--- Main.java
+++ Main.java
@@ -23,6 +23,31 @@ public class Main {
             System.out.printf("Your age is %s. You are not allowed to drive", myAge);
         }
 
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+
         /*
         %s - as a placeholder for a string,
         %n - new line,
